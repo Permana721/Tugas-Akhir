aff72d1a3875366a6e2df7cad643e4daeba148f6
Permana <sdharma721@gmail.com>
2025-01-11 16:29:16 +0700

    add search bar to dashboard

--- web/src/pages/DashboardPage.vue
+++ web/src/pages/DashboardPage.vue
@@ -18,6 +18,21 @@
             </div>
         </div>
 
+        <div class="py-16 px-6">
+            <div class="max-w-6xl mx-auto flex flex-col items-center md:flex-row md:gap-10">
+                <div class="relative w-96 h-72">
+                    <img src="/img/background/blueberry.png" alt="Blueberries" class="absolute inset-0 w-full h-full object-cover" />
+                </div>
+                <div class="mt-10 text-center md:text-left">
+                    <h2 class="text-3xl font-bold text-gray-800 mb-4">Catat lebih dari 14 juta makanan.</h2>
+                    <p class="text-lg text-gray-600">
+                        Lihat perincian kalori dan nutrisi, bandingkan ukuran porsi, dan lihat bagaimana makanan yang Anda makan mewujudkan tujuan Anda.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
         <div class="bg-white py-12 px-6">
             <div class="max-w-4xl mx-auto text-center">
                 <h2 class="text-3xl font-bold text-gray-800 mb-4">Alat untuk Target Anda</h2>
@@ -29,10 +44,10 @@
                 <div>
                     <div class="flex justify-center mb-4">
                         <div class="bg-blue-100 p-4 rounded-full">
-                            <img src="/img/icon/facebook.png" alt="Buku Harian" class="w-12 h-12" />
+                            <img src="/img/icon/open-book.png" alt="Buku Harian" class="w-12 h-12" />
                         </div>
                     </div>
-                    <h3 class="text-xl font-semibold text-gray-800 mb-2">Pelajari. Lacak. Tingkatkan.</h3>
+                    <h3 class="text-xl font-semibold text-gray-800 mb-2">Pelajari, Lacak, Tingkatkan.</h3>
                     <p class="text-gray-600">
                         Menyimpan buku harian makanan Anda akan membantu memahami kebiasaan Anda dan memperbesar kemungkinan Anda mendapatkan tujuan Anda.
                     </p>
@@ -40,7 +55,7 @@
             <div>
                 <div class="flex justify-center mb-4">
                     <div class="bg-blue-100 p-4 rounded-full">
-                        <img src="/img/icon/facebook.png" alt="Kode Batang" class="w-12 h-12" />
+                        <img src="/img/icon/scanner.png" alt="Kode Batang" class="w-12 h-12" />
                     </div>
                 </div>
                 <h3 class="text-xl font-semibold text-gray-800 mb-2">Pencatatan jadi lebih mudah.</h3>
@@ -51,7 +66,7 @@
             <div>
                 <div class="flex justify-center mb-4">
                     <div class="bg-blue-100 p-4 rounded-full">
-                        <img src="/img/icon/facebook.png" alt="Sepatu" class="w-12 h-12" />
+                        <img src="/img/icon/sneakers.png" alt="Sepatu" class="w-12 h-12" />
                     </div>
                 </div>
                 <h3 class="text-xl font-semibold text-gray-800 mb-2">Tetap Termotivasi.</h3>
@@ -71,24 +86,47 @@
             </div>
             <div class="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                 <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                    <img src="/img/background/background.png" alt="Semangkuk oatmeal dengan kranberi" class="w-full h-40 object-cover" />
+                    <img src="/img/background/menu-diet.jpeg" alt="Semangkuk oatmeal dengan kranberi" class="w-full h-40 object-cover" />
                     <div class="p-4">
                         <h3 class="text-xl font-semibold text-gray-800 mb-2">15 Sarapan Cepat di Bawah 300 Kalori</h3>
-                        <a href="#" class="text-blue-600 hover:underline">Blog MyFitnessPal</a>
+                        <a href="#" class="text-blue-600 hover:underline">Blog FUFUFAFA</a>
                     </div>
                 </div>
                 <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                    <img src="/img/background/background.png" alt="Pengaturan berbagai cheat meal" class="w-full h-40 object-cover" />
+                    <img src="/img/background/naspad.png" alt="Pengaturan berbagai cheat meal" class="w-full h-40 object-cover" />
                     <div class="p-4">
                         <h3 class="text-xl font-semibold text-gray-800 mb-2">Masalah Cheat Day</h3>
-                        <a href="#" class="text-blue-600 hover:underline">Blog MyFitnessPal</a>
+                        <a href="#" class="text-blue-600 hover:underline">Blog FUFUFAFA</a>
                     </div>
                 </div>
                 <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                    <img src="/img/background/background.png" alt="Seorang wanita jogging di kota" class="w-full h-40 object-cover" />
+                    <img src="/img/background/jogging.jpg" alt="Seorang wanita jogging di kota" class="w-full h-40 object-cover" />
                     <div class="p-4">
                         <h3 class="text-xl font-semibold text-gray-800 mb-2">Panduan Penting agar Tetap Bergerak</h3>
-                        <a href="#" class="text-blue-600 hover:underline">Blog MyFitnessPal</a>
+                        <a href="#" class="text-blue-600 hover:underline">Blog FUFUFAFA</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="px-6 py-12">
+            <div class="text-center max-w-4xl mx-auto mb-10">
+                <h1 class="text-4xl font-bold text-gray-800 mb-4">Jelajahi FUFUFAFA</h1>
+            </div>
+
+            <div class="relative bg-white rounded-lg shadow-lg mx-auto max-w-6xl p-8 md:p-16 lg:p-40 overflow-hidden">
+                <img src="/img/background/search-food.jpg" alt="Background Image" class="absolute top-0 left-0 w-full h-full rounded-lg z-0"/>
+                <div class="relative z-10 text-center bg-opacity-80 md:bg-opacity-0 p-4 md:p-0 rounded-lg">
+                    <h2 class="text-2xl md:text-4xl font-bold text-gray-800 mb-4">Cari Makanan</h2>
+                    <p class="text-sm md:text-lg mx-auto mb-7">
+                    Penasaran dengan kalori dan nutrisi dalam makanan favorit Anda? Gunakan alat pencarian kami untuk
+                    menemukan informasi nutrisi tentang makanan apa pun secara instan.
+                    </p>
+                    <div class="flex flex-col md:flex-row items-center justify-center gap-4">
+                        <input type="text" placeholder="Masukkan makanan untuk mencari" class="w-full max-w-lg px-4 py-2 border rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none"/>
+                        <button class="px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition duration-300">
+                            Cari
+                        </button>
                     </div>
                 </div>
             </div>
