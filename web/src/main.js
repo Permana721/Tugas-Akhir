e5da7a287e303110f2ebfbe6fc6e9003314da1c8
Permana <sdharma721@gmail.com>
2025-01-29 22:38:05 +0700

    add font awesome

--- web/src/main.js
+++ web/src/main.js
@@ -1,5 +1,10 @@
 import './assets/css/input.css';
 import './assets/css/style.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faEye, faEyeSlash);
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
@@ -7,6 +12,8 @@ import App from './App.vue';
 import router from './router/route';
 
 const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(createPinia());
 app.use(router);
