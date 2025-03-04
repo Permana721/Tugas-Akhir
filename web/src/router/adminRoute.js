aeebf372fe5d36c207b28a4f4df80bdf902e6e5d
Permana <sdharma721@gmail.com>
2025-03-04 13:41:51 +0700

    style: reformat code

--- web/src/router/adminRoute.js
+++ web/src/router/adminRoute.js
@@ -1,14 +1,21 @@
 import adminDashboard from '../pages/admin/adminDashboard.vue';
+
 import adminBlog from '../pages/admin/blogs/Blog.vue';
+import addBlog from '../pages/admin/blogs/addBlog.vue';
+
 import adminFood from '../pages/admin/foods/Food.vue';
+
 import adminRecipe from '../pages/admin/recipes/Recipe.vue';
+
 import adminSport from '../pages/admin/sports/Sport.vue';
+
 import adminUser from '../pages/admin/users/User.vue';
 
 const adminRoutes = [
     { path: '/admin-pg', name: 'adminDashboard', component: adminDashboard, meta: { title: 'Dashboard - Admin IP-CORP' } },
 
     { path: '/admin-pg/blog', name: 'adminBlog', component: adminBlog, meta: { title: 'Blog - Admin IP-CORP' } },
+    { path: '/admin-pg/blog/add-blog', name: 'addBlog', component: addBlog, meta: { title: 'Add Blog - Admin IP-CORP' } },
 
     { path: '/admin-pg/food', name: 'adminFood', component: adminFood, meta: { title: 'Food - Admin IP-CORP' } },
 
