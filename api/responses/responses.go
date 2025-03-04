32409df65cf8403bb7c2be87fa8db4fd49dd05de
Permana <sdharma721@gmail.com>
2025-03-05 01:13:12 +0700

    feat(responses): add BlogResponse

--- api/responses/responses.go
+++ api/responses/responses.go
@@ -1,5 +1,15 @@
 package responses
 
+type BlogResponse struct {
+	ID          *uint      `json:"id"`
+	Title       *string    `json:"title"`
+	Description *string    `json:"description"`
+	Category    *string    `json:"category"`
+	Content     *string    `json:"content"`
+	HeaderPhoto *string    `json:"header_photo"`
+	Photo       *string    `json:"photo"`
+}
+
 type UserResponse struct {	
 	ID     		*int    	`json:"id"`
 	Name 		*string 	`json:"name"`
