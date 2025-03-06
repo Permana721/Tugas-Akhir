ad3e4a1aa6abfff7de039d0e5e9e332fdc2ed87e
Permana <sdharma721@gmail.com>
2025-03-06 23:14:52 +0700

    feat: add blog and food route

--- api/routes/route.go
+++ api/routes/route.go
@@ -3,9 +3,12 @@ package routes
 import (
 	"api/configs/app_config"
 	user_controller "api/controllers/user_controller"
+    blog_controller "api/controllers/blog_controller"
+    food_controller "api/controllers/food_controller"
 	"api/middleware"
 
 	"github.com/gin-gonic/gin"
+	// "golang.org/x/tools/blog"
 )
 
 func InitRoute(app *gin.Engine) {
@@ -15,6 +18,16 @@ func InitRoute(app *gin.Engine) {
     route.Static(app_config.STATIC_ROUTE, app_config.STATIC_DIR)
     route.POST("/user/login", user_controller.Login)
 
+    route.GET("/blog", blog_controller.GetAllBlog)
+    route.GET("/blog/:id", blog_controller.GetById)
+    route.POST("/blog", blog_controller.Store)
+    route.PATCH("/blog/update/:id", blog_controller.Update)
+    route.DELETE("/blog/delete/:id", blog_controller.Delete)
+
+    route.GET("/food", food_controller.GetAllFood)
+    route.GET("/food/:id", food_controller.GetById)
+    route.POST("/food", food_controller.Store)
+
     // Lindungi route dengan middleware AuthMiddleware
     protected := route.Group("/")
     protected.Use(middleware.AuthMiddlewares())
@@ -22,7 +35,7 @@ func InitRoute(app *gin.Engine) {
         route.POST("/user/add", user_controller.Store)
         protected.GET("/user", user_controller.GetAllUser)
         protected.GET("/user/:id", user_controller.GetById)
-        protected.PATCH("/user/update/:id", user_controller.Update)
+        route.PATCH("/user/update/:id", user_controller.Update)
         protected.DELETE("/user/delete/:id", user_controller.Delete)
         protected.POST("/file", user_controller.HandleUploadFile)
     }
