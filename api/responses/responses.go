297ba1036519cba9cb55e33860f798f8769fea11
Permana <sdharma721@gmail.com>
2025-03-06 23:14:24 +0700

    feat: add FoodResponse

--- api/responses/responses.go
+++ api/responses/responses.go
@@ -6,8 +6,7 @@ type BlogResponse struct {
 	Description *string    `json:"description"`
 	Category    *string    `json:"category"`
 	Content     *string    `json:"content"`
-	HeaderPhoto *string    `json:"header_photo"`
-	Photo       *string    `json:"photo"`
+	Image 		*string    `json:"image"`
 }
 
 type UserResponse struct {	
@@ -16,4 +15,24 @@ type UserResponse struct {
 	Email     	*string  	`json:"email"`
 	Password    *string  	`json:"password"`
 	Age     	*int    	`json:"age"`
+}
+
+type FoodResponse struct {
+	ID          		*uint		`json:"id"`
+	Name        		*string 	`json:"name"`
+	Calorie  			*float64	`json:"calorie"`
+	Carbohydrate 		*float64 	`json:"carbohydrate"`
+	Category    		*string 	`json:"category"`
+	Cholesterol 		*float64 	`json:"cholesterol"`
+	Fat 				*float64 	`json:"fat"`
+	Image 				*string 	`json:"image"`
+	MonounsaturatedFat 	*float64 	`json:"monounsaturated_fat"`
+	PolyunsaturatedFat 	*float64 	`json:"polyunsaturated_fat"`
+	Portion	 			*float64 	`json:"portion"`
+	Potassium 			*float64 	`json:"potassium"`
+	Protein 			*float64 	`json:"protein"`
+	SaturatedFat 		*float64 	`json:"saturated_fat"`
+	Sodium 				*float64 	`json:"sodium"`
+	Sugar 				*float64 	`json:"sugar"`
+	Unit 				*string 	`json:"unit"`
 }
